1d022c4bb654f7d037e2ba05977a0ee13fbd1de9
iconpraveenraja <147960857+iconpraveenraja@users.noreply.github.com>
2025-06-10 16:13:18 +0200

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -1,17 +1,17 @@
 from pymongo import MongoClient
 import pandas as pd
 
-# Load CSV
+
 df = pd.read_csv("D:/car_prices.csv")
 
-# Connect to local MongoDB
+
 client = MongoClient("mongodb://localhost:27017/")
 
-# Create or connect to a database and collection
+
 db = client["AmazonDB"]
 collection = db["Products"]
 
-# Convert DataFrame to dictionary and insert
+
 collection.insert_many(df.to_dict(orient="records"))
 
 print("Data inserted successfully!")
